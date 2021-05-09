67644828c55e2eab017a3c7a93c774796c7ef788
German David <germanmarinrolong@gmail.com>
2021-05-08 22:18:30 -0500

    nuevo plugin de git

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,5 +4,11 @@ return require('packer').startup(
 	function()
 		use 'wbthomason/packer.nvim'
 		use { 'lukas-reineke/indent-blankline.nvim', branch = 'lua' }
+		use {
+			'lewis6991/gitsigns.nvim',
+			requires = {
+				'nvim-lua/plenary.nvim'
+			}
+		}
 	end
 )
