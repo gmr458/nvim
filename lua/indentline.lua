5fa876f4316fef8f486fa7bdee4e81ca6a8e186a
German David <germanmarinrolong@gmail.com>
2021-07-24 18:09:16 -0500

    indentline and lspui removed

--- lua/indentline.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd([[hi IndentBlanklineChar guifg=#2a2e36]])
-
-vim.g.indent_blankline_char = "▏"
-vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "startify", "packer"}
-vim.g.indent_blankline_buftype_exclude = {"terminal"}
-vim.g.indent_blankline_show_trailing_blankline_indent = false
