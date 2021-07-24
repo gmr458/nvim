59d877820bc2ecb751a9b5c7ba2ecabac3c4690d
German David <germanmarinrolong@gmail.com>
2021-07-24 18:05:34 -0500

    update config layout strategy

--- lua/fuzzyfinder.lua
+++ lua/fuzzyfinder.lua
@@ -1,6 +1,12 @@
 require("telescope").setup {
     defaults = {
-        layout_config = {horizontal = {preview_width = 0.7}},
+		layout_strategy = "center",
+        layout_config = {
+			center = {
+				width = 0.4,
+				height = 0.8
+			}
+		},
         file_ignore_patterns = {"node_modules", "dist", "build", "venv", "bin", "obj"},
         borderchars = {"─", "│", "─", "│", "┌", "┐", "┘", "└"},
         color_devicons = true,
@@ -10,4 +16,3 @@ require("telescope").setup {
 
 vim.api.nvim_set_keymap("", "tff", "<CMD>Telescope find_files<CR>", {noremap = false, silent = true})
 
-vim.api.nvim_set_keymap("", "tbf", "<CMD>Telescope buffers<CR>", {noremap = false, silent = true})
