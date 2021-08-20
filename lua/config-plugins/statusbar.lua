075e31460abc69e6bbbcbda2874f0cb0ee92b244
German David <germanmarinrolong@gmail.com>
2021-08-20 12:13:28 -0500

    theme = gruvbox

--- lua/config-plugins/statusbar.lua
+++ lua/config-plugins/statusbar.lua
@@ -1,7 +1,7 @@
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "nebulous",
+		theme = "gruvbox",
 		component_separators = { "", "" },
 		section_separators = { "", "" },
 		disabled_filetypes = { "NvimTree", "packer" },
