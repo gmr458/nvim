099d825b60cb9e5b2303686bce53efce7faa00f6
German David <germanmarinrolong@gmail.com>
2021-09-08 01:21:54 -0500

    LSP message removed

--- lua/config-plugins/statusbar.lua
+++ lua/config-plugins/statusbar.lua
@@ -20,25 +20,6 @@ require("lualine").setup({
                 color_modified = colors.yellow,
                 color_removed = colors.red,
             },
-            {
-                function()
-                    local msg = "No active LSP"
-                    local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-                    local clients = vim.lsp.get_active_clients()
-                    if next(clients) == nil then
-                        return msg
-                    end
-                    for _, client in ipairs(clients) do
-                        local filetypes = client.config.filetypes
-                        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                            return client.name
-                        end
-                    end
-                    return msg
-                end,
-                icon = " LSP:",
-                color = { fg = colors.fg, gui = "bold" },
-            },
         },
         lualine_x = {
             {
