668c438cb6566e452b53cde0115bf1b3c026ac96
German David <germanmarinrolong@gmail.com>
2021-09-10 22:01:20 -0500

    cursorline, relativenumber and neovide fullscreen removed

--- lua/settings.lua
+++ lua/settings.lua
@@ -8,14 +8,12 @@ set.background = "dark"
 set.tabstop = 4
 set.shiftwidth = 4
 set.expandtab = true
-set.cursorline = true
 set.omnifunc = "syntaxcomplete#Complete"
 set.ignorecase = true
 set.scrolloff = 8
 set.hidden = true
 set.errorbells = false
 set.number = true
-set.relativenumber = true
 set.wrap = false
 set.smartcase = true
 set.swapfile = false
@@ -37,7 +35,6 @@ vim.g.better_escape_shortcut = "jk"
 
 vim.cmd([[set guifont=MesloLGS\ NF]])
 vim.cmd([[let g:neovide_refresh_rate=60]])
--- vim.cmd([[let g:neovide_fullscreen=v:true]])
 vim.cmd([[let g:neovide_cursor_antialiasing=v:true]])
 
 vim.g.gruvbox_contrast_dark = "hard"
