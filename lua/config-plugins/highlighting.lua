b7b1431f171d898fbe7cfdcd2e9fba97b46e7621
German David <germanmarinrolong@gmail.com>
2021-09-12 16:41:10 -0500

    ignore install for html

--- lua/config-plugins/highlighting.lua
+++ lua/config-plugins/highlighting.lua
@@ -1,5 +1,6 @@
 require("nvim-treesitter.configs").setup({
     ensure_installed = "maintained",
+    ignore_install = { "html" },
     highlight = { enable = true },
     rainbow = { enable = true, extended_mode = true, max_file_lines = 1000 },
 })
