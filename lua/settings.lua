a6b6517c1e8f91f03c53a26ca447ad30acd2ac8a
German David <germanmarinrolong@gmail.com>
2021-11-09 18:32:24 -0500

    colorscheme enfocado

--- lua/settings.lua
+++ lua/settings.lua
@@ -71,9 +71,9 @@ end
 vim.cmd([[let g:neovide_refresh_rate=60]])
 vim.cmd([[let g:neovide_cursor_antialiasing=v:true]])
 
-vim.g.gruvbox_bold = false
+--[[ vim.g.gruvbox_bold = false
 vim.g.gruvbox_italic = false
 vim.g.gruvbox_italicize_comments = true
 vim.g.gruvbox_italicize_strings = true
-vim.g.gruvbox_contrast_dark = "hard"
-vim.cmd([[colorscheme gruvbox]])
+vim.g.gruvbox_contrast_dark = "hard" ]]
+vim.cmd([[colorscheme enfocado]])
