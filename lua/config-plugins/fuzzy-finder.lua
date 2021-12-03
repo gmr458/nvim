5947b0dcc7eefb673dbe155073b7ac14105d69a0
German David <germanmarinrolong@gmail.com>
2021-12-02 23:56:33 -0500

    config for windows

--- lua/config-plugins/fuzzy-finder.lua
+++ lua/config-plugins/fuzzy-finder.lua
@@ -1,3 +1,16 @@
+local extensions = {}
+
+if vim.fn.has("unix") == 1 then
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case" the default case_mode is "smart_case"
+        },
+    }
+end
+
 require("telescope").setup({
     defaults = {
         vimgrep_arguments = {
@@ -33,17 +46,12 @@ require("telescope").setup({
         set_env = { ["COLORTERM"] = "truecolor" },
         path_display = { "shorten" },
     },
-    extensions = {
-        fzf = {
-            fuzzy = true, -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case", -- or "ignore_case" or "respect_case" the default case_mode is "smart_case"
-        },
-    },
+    extensions = extensions
 })
 
-require("telescope").load_extension("fzf")
+if vim.fn.has("unix") == 1 then
+    require("telescope").load_extension("fzf")
+end
 
 vim.cmd([[nnoremap tff <cmd>lua require('telescope.builtin').find_files()<cr>]])
 vim.cmd([[nnoremap tfg <cmd>lua require('telescope.builtin').live_grep()<cr>]])
