5eeb331c8af55c128b00f480d9272b0312135087
German David <germanmarinrolong@gmail.com>
2021-12-07 15:44:58 -0500

    LSP for Kotlin

--- lua/config-plugins/lsp-cmp-saga.lua
+++ lua/config-plugins/lsp-cmp-saga.lua
@@ -287,6 +287,12 @@ require("lspconfig").gopls.setup({
     capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
 }) ]]
 
+-- LSP for Kotlin
+require("lspconfig").kotlin_language_server.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
 -- LSP for Python
 require("lspconfig").pyright.setup({
     on_attach = on_attach,
