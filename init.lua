cfa1cb0462481fba49fb9cc89d23dda0d328562f
German David <germanmarinrolong@gmail.com>
2021-12-24 14:50:27 -0500

    stylua format

--- init.lua
+++ init.lua
@@ -2,8 +2,8 @@ require("user.options")
 require("user.keymaps")
 require("user.plugins")
 require("user.colorscheme")
-require "user.cmp"
-require "user.lsp"
+require("user.cmp")
+require("user.lsp")
 require("user.telescope")
 require("user.treesitter")
 require("user.autopairs")
