35c7c6511988e239ae7a13fb811a01892d01f404
German David <germanmarinrolong@gmail.com>
2021-12-24 22:59:21 -0500

    config plugins formatter

---
+++ lua/user/formatter.lua
@@ -0,0 +1,68 @@
+local status_ok, formatter = pcall(require, "formatter")
+if not status_ok then
+    return
+end
+
+formatter.setup({
+    logging = false,
+    filetype = {
+        go = {
+            -- gofmt
+            function()
+                return { exe = "gofmt", args = { "-w", "." }, stdin = false }
+            end,
+        },
+        html = {
+            -- prettier
+            function()
+                return { exe = "prettier", args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--tab-width 4" }, stdin = true }
+            end,
+        },
+        javascript = {
+            -- prettier
+            function()
+                return { exe = "prettier", args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--tab-width 4" }, stdin = true }
+            end,
+        },
+        json = {
+            -- prettier
+            function()
+                return { exe = "prettier", args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--tab-width 4" }, stdin = true }
+            end,
+        },
+        jsonc = {
+            -- prettier
+            function()
+                return { exe = "prettier", args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--tab-width 4" }, stdin = true }
+            end,
+        },
+        lua = {
+            -- stylua
+            function()
+                return {
+                    exe = "stylua",
+                    args = { "--indent-type Spaces --indent-width 4 --line-endings Unix --quote-style ForceDouble" },
+                    stdin = false,
+                }
+            end,
+        },
+        python = {
+            -- autopep8
+            function()
+                return { exe = "autopep8", args = { vim.api.nvim_buf_get_name(0) }, stdin = true }
+            end,
+        },
+        rust = {
+            -- rustfmt
+            function()
+                return { exe = "rustfmt", args = { "--emit=stdout" }, stdin = true }
+            end,
+        },
+        typescript = {
+            -- prettier
+            function()
+                return { exe = "prettier", args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--tab-width 4" }, stdin = true }
+            end,
+        },
+    },
+})
