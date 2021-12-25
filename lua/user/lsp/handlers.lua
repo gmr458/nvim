3e9c5c0575200de1ca7f6fdc45360595789e4fa3
German David <germanmarinrolong@gmail.com>
2021-12-25 18:16:19 -0500

    remove automatic hiver diagnostic

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -97,9 +97,4 @@ end
 
 M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
--- You will likely want to reduce updatetime which affects CursorHold
--- note: this setting is global and should be set only once
-vim.o.updatetime = 250
-vim.cmd([[autocmd CursorHold,CursorHoldI * lua require'lspsaga.diagnostic'.show_line_diagnostics()]])
-
 return M
