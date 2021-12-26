4dbee9f7473c32d67342af16f8f18618422c2b89
German David <germanmarinrolong@gmail.com>
2021-12-25 22:22:42 -0500

    null-ls removed

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -82,7 +82,6 @@ return packer.startup(function(use)
     use("neovim/nvim-lspconfig") -- enable LSP
     use("williamboman/nvim-lsp-installer") -- simple to use language server installer
     use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for
-    use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
     use("onsails/lspkind-nvim")
     use({ "tami5/lspsaga.nvim", branch = "nvim6.0" })
 
