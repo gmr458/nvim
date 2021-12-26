3e4cb55a84c6012fbfb4005cac237969b5199d73
German David <germanmarinrolong@gmail.com>
2021-12-25 23:04:02 -0500

    term for node, deno, python and btop configured

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 toggleterm.setup({
-    direction = "float"
+    direction = "float",
 })
 
 function _G.set_terminal_keymaps()
@@ -20,32 +20,62 @@ end
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
-function _LAZYGIT_TOGGLE()
-    lazygit:toggle()
+local opts = { noremap = true, silent = true }
+
+local close_terminal = function(term)
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", opts)
 end
 
-local node = Terminal:new({ cmd = "node", hidden = true })
+local node = Terminal:new({
+    cmd = "node",
+    hidden = true,
+    on_open = function(term)
+        close_terminal(term)
+    end,
+})
 
 function _NODE_TOGGLE()
     node:toggle()
 end
 
-local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
+local deno = Terminal:new({
+    cmd = "deno repl",
+    hidden = true,
+    on_open = function(term)
+        close_terminal(term)
+    end,
+})
 
-function _NCDU_TOGGLE()
-    ncdu:toggle()
+function _DENO_TOGGLE()
+    deno:toggle()
 end
 
-local htop = Terminal:new({ cmd = "htop", hidden = true })
+local btop = Terminal:new({
+    cmd = "btop",
+    hidden = true,
+    on_open = function(term)
+        close_terminal(term)
+    end,
+})
 
-function _HTOP_TOGGLE()
-    htop:toggle()
+function _BTOP_TOGGLE()
+    btop:toggle()
 end
 
-local python = Terminal:new({ cmd = "python", hidden = true })
+local python = Terminal:new({
+    cmd = "python3",
+    hidden = true,
+    on_open = function(term)
+        close_terminal(term)
+    end,
+})
 
 function _PYTHON_TOGGLE()
     python:toggle()
 end
+
+vim.api.nvim_set_keymap("n", "<leader>node", "<cmd>lua _NODE_TOGGLE()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>deno", "<cmd>lua _DENO_TOGGLE()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>btop", "<cmd>lua _BTOP_TOGGLE()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>py", "<cmd>lua _PYTHON_TOGGLE()<CR>", opts)
