a8b93fbcd6735e375fbfd995c582f10926298c52
German David <germanmarinrolong@gmail.com>
2022-01-24 22:52:29 -0500

    package.json added

--- lua/user/lsp/settings/eslint.lua
+++ lua/user/lsp/settings/eslint.lua
@@ -1,7 +1,7 @@
 local util = require("lspconfig.util")
 
 local opts = {
-    root_dir = util.root_pattern(".eslintrc.js", ".eslintrc.cjs", ".eslintrc.yaml", ".eslintrc.yml", ".eslintrc.json"),
+    root_dir = util.root_pattern(".eslintrc.js", ".eslintrc.cjs", ".eslintrc.yaml", ".eslintrc.yml", ".eslintrc.json", "package.json"),
 }
 
 return opts
