5f2b8eb0347262465b38471002082f2ec119d8b8
German David <germanmarinrolong@gmail.com>
2022-01-24 22:52:03 -0500

    HTML snippets for JS, JSX, TS and TSX

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -111,3 +111,8 @@ cmp.setup({
         native_menu = false,
     },
 })
+
+luasnip.filetype_extend("javascript", { "html" })
+luasnip.filetype_extend("javascriptreact", { "html" })
+luasnip.filetype_extend("typescript", { "html" })
+luasnip.filetype_extend("typescriptreact", { "html" })
