14cb95a69c64762075450c3f509d5f01463c033e
German David <germanmarinrolong@gmail.com>
2022-01-26 18:20:25 -0500

    italic disabled

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,13 +1,13 @@
 -- settings gruvbox
 vim.g.gruvbox_bold = false
-vim.g.gruvbox_italic = true
+vim.g.gruvbox_italic = false
 vim.g.gruvbox_transparent_bg = true
 vim.g.gruvbox_underline = false
 vim.g.gruvbox_undercurl = false
 vim.g.gruvbox_contrast_dark = "hard"
 vim.g.gruvbox_contrast_light = "hard"
-vim.g.gruvbox_italicize_comments = true
-vim.g.gruvbox_italicize_strings = true
+vim.g.gruvbox_italicize_comments = false
+vim.g.gruvbox_italicize_strings = false
 
 vim.cmd([[
     try
