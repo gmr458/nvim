b8244da3e13c7b87fe7b527eaa440bd23cd42eea
German David <germanmarinrolong@gmail.com>
2022-01-27 23:58:23 -0500

    cmds moved from options.lua

--- lua/user/cmds.lua
+++ lua/user/cmds.lua
@@ -1,3 +1,14 @@
+vim.cmd("set whichwrap+=<,>,[,],h,l")
+vim.cmd([[set iskeyword+=-]])
+vim.cmd([[set formatoptions-=cro]])
+vim.cmd([[
+    augroup RestoreCursorShapeOnExit
+        autocmd!
+        autocmd VimLeave * set guicursor=a:hor20
+    augroup END
+]])
+vim.cmd("au BufRead,BufNewFile tsconfig.json set filetype=jsonc")
 vim.cmd("let &fcs='eob: '")
 vim.cmd("hi SignColumn guibg=none")
 vim.cmd([[set fillchars+=vert:\ ]])
+vim.cmd([[autocmd BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif]])
