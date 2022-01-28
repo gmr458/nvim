3418c12ee190a12d4254d50ed0c0c01743f65874
German David <germanmarinrolong@gmail.com>
2022-01-28 00:00:18 -0500

    theme catppuccin configured

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,19 +1,67 @@
--- settings gruvbox
-vim.g.gruvbox_bold = false
-vim.g.gruvbox_italic = false
-vim.g.gruvbox_transparent_bg = true
-vim.g.gruvbox_underline = false
-vim.g.gruvbox_undercurl = false
-vim.g.gruvbox_contrast_dark = "hard"
-vim.g.gruvbox_contrast_light = "hard"
-vim.g.gruvbox_italicize_comments = false
-vim.g.gruvbox_italicize_strings = false
+local status_ok, catppuccin = pcall(require, "catppuccin")
+if not status_ok then
+    return
+end
 
-vim.cmd([[
-    try
-        colorscheme gruvbox
-    catch /^Vim\%((\a\+)\)\=:E185/
-        colorscheme default
-        set background=dark
-    endtry
-]])
+catppuccin.setup({
+    transparent_background = false,
+    term_colors = true,
+    styles = {
+        comments = "italic",
+        functions = "NONE",
+        keywords = "italic",
+        strings = "NONE",
+        variables = "NONE",
+    },
+    integrations = {
+        treesitter = true,
+        native_lsp = {
+            enabled = true,
+            virtual_text = {
+                errors = "italic",
+                hints = "italic",
+                warnings = "italic",
+                information = "italic",
+            },
+            underlines = {
+                errors = "underline",
+                hints = "underline",
+                warnings = "underline",
+                information = "underline",
+            },
+        },
+        lsp_trouble = false,
+        cmp = true,
+        lsp_saga = true,
+        gitgutter = false,
+        gitsigns = true,
+        telescope = true,
+        nvimtree = {
+            enabled = true,
+            show_root = true,
+            transparent_panel = false,
+        },
+        which_key = false,
+        indent_blankline = {
+            enabled = false,
+            colored_indent_levels = false,
+        },
+        dashboard = false,
+        neogit = false,
+        vim_sneak = false,
+        fern = false,
+        barbar = false,
+        bufferline = true,
+        markdown = true,
+        lightspeed = false,
+        ts_rainbow = true,
+        hop = false,
+        notify = false,
+        telekasten = false,
+    },
+})
+
+local colors = require("catppuccin.api.colors").get_colors()
+catppuccin.remap({ NvimTreeVertSplit = { fg = colors.black1, bg = colors.black1 } })
+
+vim.cmd([[colorscheme catppuccin]])
