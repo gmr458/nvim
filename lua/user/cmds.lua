2a72ddc06c19a7b484f6164cef94a408b742cb35
German David <germanmarinrolong@gmail.com>
2022-02-03 18:22:37 -0500

    refactoring autocmds

--- lua/user/cmds.lua
+++ lua/user/cmds.lua
@@ -2,13 +2,16 @@ vim.cmd("set whichwrap+=<,>,[,],h,l")
 vim.cmd([[set iskeyword+=-]])
 vim.cmd([[set formatoptions-=cro]])
 vim.cmd([[
-    augroup RestoreCursorShapeOnExit
-        autocmd!
-        autocmd VimLeave * set guicursor=a:hor20
-    augroup END
+  augroup RestoreCursorShapeOnExit
+    autocmd!
+    autocmd VimLeave * set guicursor=a:hor20
+  augroup END
 ]])
 vim.cmd("au BufRead,BufNewFile tsconfig.json set filetype=jsonc")
 vim.cmd("let &fcs='eob: '")
 vim.cmd("hi SignColumn guibg=none")
 vim.cmd([[set fillchars+=vert:\ ]])
-vim.cmd([[autocmd BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | elseif bufname('%') == "" | set laststatus=0 | elseif &filetype == "toggleterm" | set laststatus=0 | else | set laststatus=2 | endif]])
+vim.cmd([[
+  autocmd BufEnter,BufWinEnter,WinEnter,CmdwinEnter *
+    \ let &laststatus = bufname('%') == "NvimTree" || bufname('%') == "" || &filetype == "toggleterm" ? 0 : 2
+]])
