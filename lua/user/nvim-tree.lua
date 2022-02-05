41c6d6a9b369d15e28320c8d742f3bb4adec163c
German David <germanmarinrolong@gmail.com>
2022-02-04 19:00:05 -0500

    update config

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,6 +1,5 @@
-vim.g.nvim_tree_indent_markers = 0 -- 0 by default, this option shows indent markers when folders are open
-vim.g.nvim_tree_root_folder_modifier = ":t" -- This is the default. See :help filename-modifiers for more options
-vim.g.nvim_tree_group_empty = 1 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
+vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_group_empty = 1
 vim.g.nvim_tree_window_picker_exclude = {
     filetype = {
         "notify",
@@ -11,33 +10,22 @@ vim.g.nvim_tree_window_picker_exclude = {
         "terminal",
     },
 }
--- Dictionary of buffer option names mapped to a list of option values that
--- indicates to the window picker that the buffer's window should not be
--- selectable.
-vim.g.nvim_tree_special_files = {} -- List of filenames that gets highlighted with NvimTreeSpecialFile
+vim.g.nvim_tree_special_files = {}
 vim.g.nvim_tree_show_icons = {
     git = 1,
     folders = 1,
     files = 1,
     folder_arrows = 1,
 }
--- If 0, do not show the icons for one of 'git' 'folder' and 'files'
--- 1 by default, notice that if 'files' is 1, it will only display
--- if nvim-web-devicons is installed and on your runtimepath.
--- if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
--- but this will not work when you set indent_markers (because of UI conflict)
-
--- default will show icon by default if no icon is provided
--- default shows no icon by default
 vim.g.nvim_tree_icons = {
     default = "",
     symlink = "",
     git = {
-        unstaged = "",
-        staged = "S",
+        unstaged = "!",
+        staged = "+",
         unmerged = "",
         renamed = "➜",
-        untracked = "U",
+        untracked = "?",
         deleted = "",
         ignored = "◌",
     },
@@ -66,10 +54,21 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup({
+    diagnostics = {
+        enable = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
+    },
     filters = {
         custom = { ".git" },
     },
     view = {
+        hide_root_folder = true,
+        auto_resize = true,
         mappings = {
             list = {
                 { key = "s", cb = tree_cb("vsplit") },
