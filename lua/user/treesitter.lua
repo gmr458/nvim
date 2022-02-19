e0769d5877f45635407f6ca2df8bed42fe5f9ef8
German David <germanmarinrolong@gmail.com>
2022-02-18 22:18:12 -0500

    minor update

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -10,10 +10,16 @@ configs.setup({
     autopairs = {
         enable = true,
     },
+    autotag = {
+        enable = true,
+    },
     highlight = {
         enable = true, -- false will disable the whole extension
         disable = { "" }, -- list of language that will be disabled
         additional_vim_regex_highlighting = true,
+    },
+    indent = {
+        enable = true,
     },
     context_commentstring = {
         enable = true,
