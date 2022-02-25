0e0a9cbc305f815f226e489374fabc6f2eeeafe0
German David <germanmarinrolong@gmail.com>
2022-02-24 21:06:49 -0500

    update colorschemes config

--- lua/user/colorschemes/github.lua
+++ lua/user/colorschemes/github.lua
@@ -3,4 +3,19 @@ if not status_ok then
     return
 end
 
-github.setup({ theme_style = "dark_default" })
+github.setup({
+    theme_style = "dark_default",
+    dark_sidebar = true,
+    comment_style = "italic",
+    keyword_style = "italic",
+    function_style = "italic",
+    variable_style = "NONE",
+    overrides = function(c)
+        -- local config = require("github-theme.config")
+        return {
+            VertSplit = { bg = c.bg_sidebar },
+            StatusLine = { bg = c.bg_sidebar },
+            StatusLineNC = { bg = c.bg_sidebar },
+        }
+    end,
+})
--- lua/user/colorschemes/material.lua
+++ lua/user/colorschemes/material.lua
@@ -3,16 +3,31 @@ if not status_ok then
     return
 end
 
+vim.g.material_style = "deep ocean"
+
+local colors = require("material.colors")
+
+local nvimtree = true
+
 material.setup({
     contrast = {
-        sidebars = true,
+        sidebars = nvimtree,
     },
     italics = {
         comments = true,
         keywords = true,
     },
+    custom_highlights = {
+        NvimTreeVertSplit = {
+            bg = nvimtree and colors.contrast or colors.sidebar,
+        },
+        NvimTreeStatusLine = {
+            bg = nvimtree and colors.contrast or colors.sidebar,
+        },
+        NvimTreeStatuslineNc = {
+            bg = nvimtree and colors.contrast or colors.sidebar,
+        },
+    },
 })
-
-vim.g.material_style = "deep ocean"
 
 vim.cmd("colorscheme material")
