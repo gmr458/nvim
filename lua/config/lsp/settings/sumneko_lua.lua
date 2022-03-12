e184025ca09a5a6743dfc86bd619b400d2944bb9
German David <germanmarinrolong@gmail.com>
2022-03-12 15:52:16 -0500

    update lua lsp server config

--- lua/config/lsp/settings/sumneko_lua.lua
+++ lua/config/lsp/settings/sumneko_lua.lua
@@ -1,15 +1,10 @@
 return {
     settings = {
         Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-            workspace = {
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.stdpath("config") .. "/lua"] = true,
-                },
-            },
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+            telemetry = { enable = false },
         },
     },
 }
