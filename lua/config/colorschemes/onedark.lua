277dd1c591b817a0b3593ba78c21f2659d73665b
German David <germanmarinrolong@gmail.com>
2022-03-13 19:06:09 -0500

    onedark config added

---
+++ lua/config/colorschemes/onedark.lua
@@ -0,0 +1,38 @@
+local status_ok, onedark = pcall(require, "onedark")
+
+if not status_ok then
+    return
+end
+
+onedark.setup({
+    style = "deep", -- Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    transparent = false,
+    term_colors = true,
+    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+    toggle_style_key = "<leader>ts",
+    toggle_style_list = {
+        "dark",
+        "darker",
+        "cool",
+        "deep",
+        "warm",
+        "warmer",
+        "light",
+    },
+    code_style = {
+        comments = "italic",
+        keywords = "none",
+        functions = "none",
+        strings = "none",
+        variables = "none",
+    },
+    colors = {}, -- Override default colors
+    highlights = {}, -- Override highlight groups
+    diagnostics = {
+        darker = true, -- darker colors for diagnostic
+        undercurl = true, -- use undercurl instead of underline for diagnostics
+        background = true, -- use background color for virtual text
+    },
+})
+
+onedark.load()
