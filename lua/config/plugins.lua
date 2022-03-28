b31843afd4c89bd79fe91df5098d13d72eca0c1a
German David <germanmarinrolong@gmail.com>
2022-03-27 23:44:09 -0500

    filetype disabled

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -42,7 +42,7 @@ return packer.startup(function(use)
     use("wbthomason/packer.nvim")
 
     use("lewis6991/impatient.nvim")
-    use("nathom/filetype.nvim")
+    -- use("nathom/filetype.nvim")
 
     use("nvim-lua/popup.nvim")
     use("nvim-lua/plenary.nvim")
