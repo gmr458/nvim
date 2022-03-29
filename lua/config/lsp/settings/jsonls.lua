5442fbe6e4788ddee1cf3d41ab4bf7226416254c
German David <germanmarinrolong@gmail.com>
2022-03-29 12:10:47 -0500

    fix JSON config

--- lua/config/lsp/settings/jsonls.lua
+++ lua/config/lsp/settings/jsonls.lua
@@ -1,6 +1,21 @@
 local status_ok, schemastore = pcall(require, "schemastore")
+
 if not status_ok then
-    return
+    return {
+        setup = {
+            commands = {
+                Format = {
+                    function()
+                        vim.lsp.buf.range_formatting(
+                            {},
+                            { 0, 0 },
+                            { vim.fn.line("$"), 0 }
+                        )
+                    end,
+                },
+            },
+        },
+    }
 end
 
 return {
