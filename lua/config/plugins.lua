d81256e9b827021f7cdfff3c51d3fec8664c978a
German David <germanmarinrolong@gmail.com>
2022-03-31 20:20:45 -0500

    lazyload for lsp with event

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -154,7 +154,10 @@ return packer.startup(function(use)
     use("nvim-telescope/telescope-media-files.nvim")
 
     -- LSP
-    use("williamboman/nvim-lsp-installer")
+    use({
+        "williamboman/nvim-lsp-installer",
+        event = "BufReadPre",
+    })
     use({
         "neovim/nvim-lspconfig",
         after = "nvim-lsp-installer",
