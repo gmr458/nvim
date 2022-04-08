f66b3993af3d6957a2dbd7fc521bb400579779c4
German David <germanmarinrolong@gmail.com>
2022-04-08 13:04:52 -0500

    nvim tree background

--- lua/config/colorschemes/kanagawa.lua
+++ lua/config/colorschemes/kanagawa.lua
@@ -4,6 +4,8 @@ if not status_ok then
     return
 end
 
+local default_colors = require("kanagawa.colors").setup()
+
 kanagawa.setup({
     undercurl = true,
     commentStyle = "NONE",
@@ -18,7 +20,10 @@ kanagawa.setup({
     dimInactive = false,
     globalStatus = true,
     colors = {},
-    overrides = {},
+    overrides = {
+        NvimTreeNormal = { bg = default_colors.sumiInk0 },
+        NvimTreeNormalNC = { link = "NvimTreeNormal" },
+    },
 })
 
 vim.cmd("colorscheme kanagawa")
