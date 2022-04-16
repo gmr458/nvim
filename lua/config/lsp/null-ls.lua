56bb99e4061a448197fe618107f6115407993762
German David <germanmarinrolong@gmail.com>
2022-04-16 17:40:43 -0500

    null-ls.nvim added and formatter.nvim removed

---
+++ lua/config/lsp/null-ls.lua
@@ -0,0 +1,15 @@
+local status_ok, null_ls = pcall(require, "null-ls")
+
+if not status_ok then
+    return
+end
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.rustfmt,
+        null_ls.builtins.formatting.stylua,
+    },
+})
