2254ce88d4f895e22bd4ecfcbf2819842c050805
German David <germanmarinrolong@gmail.com>
2022-05-03 11:27:47 -0500

    comment playground

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -125,10 +125,10 @@ return packer.startup(function(use)
         run = ":TSUpdate",
         config = "require('config.treesitter')",
     })
-    use({
-        "nvim-treesitter/playground",
-        after = "nvim-treesitter",
-    })
+    -- use({
+    --     "nvim-treesitter/playground",
+    --     after = "nvim-treesitter",
+    -- })
     use({
         "p00f/nvim-ts-rainbow",
         ft = filetypes,
