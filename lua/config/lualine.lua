ceb08a49b7a6826f22c9696437a40b880553d141
German David <germanmarinrolong@gmail.com>
2022-05-03 18:13:03 -0500

    theme gruvbox-material

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -7,7 +7,7 @@ end
 lualine.setup({
     options = {
         icons_enabled = true,
-        theme = "auto",
+        theme = "gruvbox-material",
         component_separators = "",
         section_separators = "",
         disabled_filetypes = {
