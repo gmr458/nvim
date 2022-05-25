0034bc542b45764784411a24413b4e9814c5c223
German David <germanmarinrolong@gmail.com>
2022-05-24 22:29:41 -0500

    final update enfocado

--- lua/config/colorschemes/enfocado.lua
+++ lua/config/colorschemes/enfocado.lua
@@ -14,15 +14,35 @@ vim.cmd([[
 
     augroup enfocado_customization
         autocmd!
+            " Float windows
             autocmd ColorScheme enfocado highlight NormalFloat guibg=#181818
             autocmd ColorScheme enfocado highlight FloatBorder guibg=#181818
+
+            " Diagnostic floating windows
             autocmd ColorScheme enfocado highlight DiagnosticFloatingError guibg=#181818
             autocmd ColorScheme enfocado highlight DiagnosticFloatingHint guibg=#181818
             autocmd ColorScheme enfocado highlight DiagnosticFloatingInfo guibg=#181818
             autocmd ColorScheme enfocado highlight DiagnosticFloatingWarn guibg=#181818
+
+            " Statusline
             autocmd ColorScheme enfocado highlight StatusLine guibg=#181818
-            autocmd ColorScheme enfocado highlight TelescopeSelection guibg=#181818 guifg=#368AEB
-            autocmd ColorScheme enfocado highlight TelescopeMultiSelection guibg=#181818
+
+            " Telescope
+            autocmd ColorScheme enfocado highlight TelescopeBorder guibg=#121212 guifg=#121212
+            autocmd ColorScheme enfocado highlight TelescopeMultiSelection guibg=#121212
+            autocmd ColorScheme enfocado highlight TelescopeNormal guibg=#121212
+            autocmd ColorScheme enfocado highlight TelescopePreviewBorder guibg=#121212 guifg=#121212
+            autocmd ColorScheme enfocado highlight TelescopePreviewNormal guibg=#121212
+            autocmd ColorScheme enfocado highlight TelescopePreviewTitle guibg=#121212
+            autocmd ColorScheme enfocado highlight TelescopePromptBorder guibg=#121212 guifg=#121212
+            autocmd ColorScheme enfocado highlight TelescopePromptNormal guibg=#121212
+            autocmd ColorScheme enfocado highlight TelescopePromptTitle guibg=#121212 guifg=#777777
+            autocmd ColorScheme enfocado highlight TelescopeResultsBorder guibg=#121212 guifg=#121212
+            autocmd ColorScheme enfocado highlight TelescopeResultsNormal guibg=#121212
+            autocmd ColorScheme enfocado highlight TelescopeResultsTitle guifg=#777777
+            autocmd ColorScheme enfocado highlight TelescopeSelection guibg=#121212 guifg=#368AEB
+
+            " NvimTree
             autocmd ColorScheme enfocado highlight NvimTreeNormal guibg=#0D0D0D
             autocmd ColorScheme enfocado highlight! link NvimTreeNormalNC NvimTreeNormal
             autocmd ColorScheme enfocado highlight NvimTreeWinSeparator guibg=#181818 guifg=#181818
