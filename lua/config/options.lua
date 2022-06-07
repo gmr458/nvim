79318d1050491d17aa4ffdeed8864bb1510a9ac1
German David <germanmarinrolong@gmail.com>
2022-06-07 15:48:19 -0500

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -55,8 +55,8 @@ vim.g.neovide_refresh_rate = 60
 vim.g.neovide_fullscreen = true
 vim.g.neovide_input_use_logo = true
 vim.g.neovide_profiler = false
-vim.g.neovide_cursor_animation_length = 0.1
-vim.g.neovide_cursor_trail_length = 0.1
+-- vim.g.neovide_cursor_animation_length = 0.1
+-- vim.g.neovide_cursor_trail_length = 0.01
 vim.g.neovide_cursor_antialiasing = true
 
 vim.cmd([[
