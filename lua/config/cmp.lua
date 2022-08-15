46ed435c73e77575271d7cf0481c7d3907c6d591
German David <germanmarinrolong@gmail.com>
2022-08-14 20:13:54 -0500

    update

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -11,18 +11,102 @@ require("luasnip.loaders.from_vscode").lazy_load({
 
 local cmp_loaded, cmp = pcall(require, "cmp")
 
+print("cmp not loaded")
 if not cmp_loaded then
-    print("cmp not loaded")
     return
 end
 
+if cmp == nil then
+    print("cmp is nil")
+    return
+end
+
+local types = require("cmp.types")
+
+local function deprioritize_snippet(entry1, entry2)
+    if entry1:get_kind() == types.lsp.CompletionItemKind.Snippet then
+        return false
+    end
+    if entry2:get_kind() == types.lsp.CompletionItemKind.Snippet then
+        return true
+    end
+end
+
+local function prioritize_keyword(entry1, entry2)
+    if entry1:get_kind() ~= types.lsp.CompletionItemKind.Keyword then
+        return false
+    end
+    if entry2:get_kind() ~= types.lsp.CompletionItemKind.Keyword then
+        return true
+    end
+end
+
 cmp.setup({
-    preselect = cmp.PreselectMode.None,
+    preselect = types.cmp.PreselectMode.None,
+    mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        -- ["<C-c>"] = cmp.mapping({
+        --     i = cmp.mapping.abort(),
+        --     c = cmp.mapping.close(),
+        -- }),
+        ["<Esc>"] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        ["<CR>"] = cmp.mapping.confirm({
+            behavior = types.cmp.ConfirmBehavior.Replace,
+            select = true,
+        }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    }),
     snippet = {
         expand = function(args)
             luasnip.lsp_expand(args.body)
         end,
     },
+    sorting = {
+        comparators = {
+            deprioritize_snippet,
+            prioritize_keyword,
+            -- cmp.config.compare.offset,
+            -- cmp.config.compare.exact,
+            -- cmp.config.compare.score,
+            -- cmp.config.compare.recently_used,
+            cmp.config.compare.kind,
+            -- cmp.config.compare.sort_text,
+            -- cmp.config.compare.length,
+            -- cmp.config.compare.order,
+            -- cmp.config.compare.locality,
+            -- cmp.config.compare.scopes,
+        },
+    },
+    sources = cmp.config.sources({
+        { name = "nvim_lua" },
+        { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "luasnip" },
+        { name = "path" },
+    }, {
+        { name = "buffer" },
+    }),
+    confirmation = { default_behavior = types.cmp.ConfirmBehavior.Replace },
     window = {
         completion = {
             border = {
@@ -50,48 +134,6 @@ cmp.setup({
             },
             winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
         },
-    },
-    mapping = cmp.mapping.preset.insert({
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-c>"] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-        }),
-        ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-    }),
-    sources = cmp.config.sources({
-        { name = "nvim_lua" },
-        { name = "nvim_lsp" },
-        { name = "nvim_lsp_signature_help" },
-        { name = "luasnip" },
-        { name = "path" },
-    }, {
-        { name = "buffer" },
-    }),
-    confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
     },
 })
 
