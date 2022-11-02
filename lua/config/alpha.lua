bfb0cabf3f08c7676d100a5fc7b228285476ebf2
German David <germanmarinrolong@gmail.com>
2022-11-02 17:07:11 -0500

    update

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -13,6 +13,8 @@ dashboard.section.buttons.val = {
     dashboard.button("tff", "  Find file", "<CMD>silent Telescope find_files hidden=true no_ignore=true<CR>"),
     dashboard.button("tfg", "  Live grep", "<CMD>silent Telescope live_grep preview=true<CR>"),
     dashboard.button("tfr", "  Recent File", "<CMD>silent Telescope oldfiles preview=false<CR>"),
+    dashboard.button("cfg", "  Config", "<CMD>silent cd ~/.config/nvim | e init.lua<CR>"),
+    dashboard.button("upd", "ﮮ  Update plugins", "<CMD>PackerSync<CR>"),
     dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
 }
 
