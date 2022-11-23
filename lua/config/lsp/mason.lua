69afca5379da694157c76ac293f51f5b7a02d3d4
German David <germanmarinrolong@gmail.com>
2022-11-23 01:46:35 -0500

    update

--- lua/config/lsp/mason.lua
+++ lua/config/lsp/mason.lua
@@ -6,7 +6,14 @@ if not mason_loaded then
 end
 
 mason.setup({
+    -- Where Mason should put its bin location in your PATH. Can be one of:
+    -- - "prepend" (default, Mason's bin location is put first in PATH)
+    -- - "append" (Mason's bin location is put at the end of PATH)
+    -- - "skip" (doesn't modify PATH)
+    ---@type '"prepend"' | '"append"' | '"skip"'
+    PATH = "append",
     ui = {
+        -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
         border = {
             "┌",
             "─",
@@ -18,5 +25,4 @@ mason.setup({
             "│",
         },
     },
-    -- max_concurrent_installers = 1,
 })
