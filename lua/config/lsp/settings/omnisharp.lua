ad568f5ea87c408845eb5bbbb33cec3b2490a6e8
German David <germanmarinrolong@gmail.com>
2022-12-29 21:35:06 -0500

    update

--- lua/config/lsp/settings/omnisharp.lua
+++ lua/config/lsp/settings/omnisharp.lua
@@ -1,13 +1,13 @@
 local env_var_user_dir = "HOME"
 local omnisharp_path = "/.local/share/nvim/mason/packages/omnisharp/"
 
-if vim.fn.has("win32") then
+if vim.fn.has("win32") == 1 then
     env_var_user_dir = "USERPROFILE"
-    omnisharp_path = "\AppData\Local\nvim-data\mason\packages\omnisharp\"
+    omnisharp_path = "\\AppData\\Local\\nvim-data\\mason\\packages\\omnisharp\\"
 end
 
 local user_directory = os.getenv(env_var_user_dir)
 
-return {
-    cmd = { "dotnet", user_directory .. omnisharp_path .."OmniSharp.dll" },
-}
+local omnisharp_dll_path = user_directory .. omnisharp_path .. "OmniSharp.dll"
+
+return { cmd = { "dotnet", omnisharp_dll_path } }
