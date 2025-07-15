17204e7284162b196d794623fdaf18e2c2ad2381
German David <germanmarinrolong@gmail.com>
2025-07-14 20:35:00 -0500

    update

--- lua/gmr/core/commands.lua
+++ lua/gmr/core/commands.lua
@@ -7,3 +7,8 @@ vim.api.nvim_create_user_command('CopyCurrentFilename', function()
     vim.fn.system('echo -n ' .. filename .. ' | wl-copy')
     vim.notify(filename .. ' copied', vim.log.levels.INFO)
 end, {})
+
+vim.api.nvim_create_user_command('DeleteAllBuffers', function()
+    local bufs = vim.api.nvim_list_bufs()
+    vim.cmd('bdelete ' .. table.concat(bufs, ' '))
+end, {})
