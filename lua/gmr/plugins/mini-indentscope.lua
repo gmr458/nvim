d0edad81ae9e275a96989f1b79cfde2a85192e94
German David <germanmarinrolong@gmail.com>
2025-09-21 20:27:06 -0500

    change echasnovski to nvim-mini

--- lua/gmr/plugins/mini-indentscope.lua
+++ lua/gmr/plugins/mini-indentscope.lua
@@ -1,5 +1,5 @@
 return {
-    'echasnovski/mini.indentscope',
+    'nvim-mini/mini.indentscope',
     version = false,
     event = 'BufReadPost',
     config = function()
--- lua/gmr/plugins/mini-surround.lua
+++ lua/gmr/plugins/mini-surround.lua
@@ -1,5 +1,5 @@
 return {
-    'echasnovski/mini.surround',
+    'nvim-mini/mini.surround',
     event = 'InsertEnter',
     version = false,
     config = function()
